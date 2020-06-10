0cec60d7a6a0a2dc6ef346a2e6df199833a3f138
Hank8933 <48377489+Hank8933@users.noreply.github.com>
2020-06-10 20:35:53 +0800

    clean debug

--- main.c
+++ main.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAX 101
-//#define DEBUG
-//  x + 1  x^2 + 3
+
 typedef struct TERM {
     int degree;
     int coef;
@@ -19,8 +18,7 @@ void getPoly(term a[])
     j = 0;\
     for (;;){\
         int i = 0;\
-        while (' ' != (numc[i] = getchar()) && numc[i++] != '\n'){\
-        }\
+        while (' ' != (numc[i] = getchar()) && numc[i++] != '\n');\
         t[j++].item = atoi(numc);\
         if (numc[i - 1] == '\n') break;\
     }
@@ -28,7 +26,7 @@ void getPoly(term a[])
     get(degree)
     get(coef)
 
-    for (int i = t[0].degree, k = 0, h = 0; i >= 0; i--, k++){
+    for (int i = t[0].degree, k = 0, h = 0; i >= 0; i--, k++){  //補缺項，i = 次方，k = 補0後的項編號，h = 補0前的項編號
         if (i == t[h].degree)
             a[k] = t[h++];
         else
@@ -44,70 +42,22 @@ int main()
     scanf("%d", &n);
     getchar();
     for (int i = 0; i < n; i++){
-        memset(polyA, 0, MAX * s);
+        memset(polyA, 0, MAX * s);  //用0字元填滿poly陣列來洗白
         memset(polyB, 0, MAX * s);
         memset(polyAnsA, 0, MAX * s);
 
         getPoly(polyA);
         getPoly(polyB);
 
-        #ifdef DEBUG
-            printf("\n\n=========INPUT_DEBUG=========\n");
-            for (int j = 0; j < 20; j++){
-                printf("%3d ", polyA[j].degree);
-            }
-            printf("\n");
-            for (int j = 0; j < 20; j++){
-                printf("%3d ", polyA[j].coef);
-            }
-            printf("\n");
-            for (int j = 0; j < 20; j++){
-                printf("%3d ", polyB[j].degree);
-            }
-            printf("\n");
-            for (int j = 0; j < 20; j++){
-                printf("%3d ", polyB[j].coef);
-            }
-            printf("\n=========INPUT_DEBUG=========\n\n");
-        #endif // DEBUG
-
-        int diff = polyA[0].degree - polyB[0].degree;
+        int diff = polyA[0].degree - polyB[0].degree;  //次方差
         for (int j = 0; j <= diff; j++){
-            polyAnsA[j].degree = diff - j;
-            polyAnsA[j].coef = polyA[j].coef / polyB[0].coef;
+            polyAnsA[j].degree = diff - j;  //商
+            polyAnsA[j].coef = polyA[j].coef / polyB[0].coef;  //商
 
             for (int k = 0; polyB[k].degree || polyB[k].coef; k++){
-                polyA[k + j].coef -= polyB[k].coef * polyAnsA[j].coef;
+                polyA[k + j].coef -= polyB[k].coef * polyAnsA[j].coef;  //餘式
             }
-
-            #ifdef DEBUG
-                printf("\n\n=========DEBUG=========\n");
-                for (int p = 0; p < 20; p++){
-                    printf("%3d ", polyA[p].coef);
-                }
-                printf("\n=========DEBUG=========\n\n");
-            #endif // DEBUG
         }
-
-        #ifdef DEBUG
-            printf("\n\n=========OUTPUT_DEBUG=========\n");
-            for (int j = 0; j < 20; j++){
-                printf("%3d ", polyAnsA[j].degree);
-            }
-            printf("\n");
-            for (int j = 0; j < 20; j++){
-                printf("%3d ", polyAnsA[j].coef);
-            }
-            printf("\n\n");
-            for (int j = 0; j < 20; j++){
-                printf("%3d ", polyA[j].degree);
-            }
-            printf("\n");
-            for (int j = 0; j < 20; j++){
-                printf("%3d ", polyA[j].coef);
-            }
-            printf("\n=========OUTPUT_DEBUG=========\n\n");
-        #endif // DEBUG
 
         int flag;
         #define show(tag, item)\
